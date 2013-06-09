3dc944a4b01b91ff5c62c806240b02a0df21143d
saaifulislam@gmail.com <saaifulislam@gmail.com@dbf5543e-da1a-c199-ca29-189bca3f3b27>
2013-06-09 16:41:48 +0000

    Database Config File

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+$dbname="LMS"; //database Name
+$dbhost="localhost"; //database Host
+$dbuser="root"; //database user name
+$dbpass=""; //database Passwoed
+$max="7"; //Max Day For Book Return
+$name="LMS Free"; //Project/Site Name
+$user="admin"; //Username For Admin
+$password="123";//Password Word For Admin
+$salt="45&^8#@1)(_+";
+?>
